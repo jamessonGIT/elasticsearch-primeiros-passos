4dd88f613f5bd99bf37a3e84537bd3ed0279fcfb
jamessonGIT <jamesson.usaf@gmail.com>
2025-11-19 03:09:02 -0300

    Arquivo python de conexão
    Testa a conexão com o elasticsearch localmente.

---
+++ conexao.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# 
+
+#sudo apt install -y python3 python3-elasticsearch
+
+#Importando a biblioteca Elasticsearch
+
+from elasticsearch import Elasticsearch as es 
+
+es = Elasticsearch(hosts = ["http://localhost:9200"], 
+                   basic_auth=("elastic", "nY5AQz37ZZIfMev9nY5AQz37ZZIfMev9")
+                   
+                   
+                    )
+
+print(es.info())
+
+#curl -XGET "localhost:9200/_cluster/health?pretty"
+
+#curl -XGET "localhost:9200/_cat/indices?v" 
+
+#curl -XGET "localhost:9200/nome_do_indice/_search?pretty" -H 'Content-Type: application/json' -d'
