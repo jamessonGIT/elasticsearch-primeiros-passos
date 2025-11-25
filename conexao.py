a84967e8f608a6554ac3e9c08c7186bf581341b4
jamessonGIT <jamesson.usaf@gmail.com>
2025-11-25 08:43:54 -0300

    Refactor Elasticsearch connection variable name
    feito.

--- conexao.py
+++ conexao.py
@@ -4,17 +4,18 @@
 
 from elasticsearch import Elasticsearch as es 
 
-es = Elasticsearch(hosts = ["http://localhost:9200"], 
+es_status = es(hosts = ["http://localhost:9200"], 
                    basic_auth=("elastic", "nY5AQz37ZZIfMev9nY5AQz37ZZIfMev9")
                    
                    
                     )
 
-print(es.info())
+print(es_status.info())
 
 #curl -XGET "localhost:9200/_cluster/health?pretty"
 
 #curl -XGET "localhost:9200/_cat/indices?v" 
 
 #curl -XGET "localhost:9200/nome_do_indice/_search?pretty" -H 'Content-Type: application/json' -d'
+
 
