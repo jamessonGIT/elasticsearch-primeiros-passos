def33a1c8b8c481aaa0837210b2517649c3a9a2e
jamessonGIT <jamesson.usaf@gmail.com>
2025-12-02 16:46:02 -0300

    Refactor ETL/search.py for clarity and structure

--- ETL/search.py
+++ ETL/search.py
@@ -2,12 +2,11 @@
 from elasticsearch import Elasticsearch
 import json
 
+#=================================Cria Conexão com ElasticSearch============================================
 es = Elasticsearch(hosts = ["http://localhost:9200"], 
                    basic_auth=("elastic", "nY5AQz37ZZIfMev9nY5AQz37ZZIfMev9")
-                   
-                   
+                              
                     )
-
 es_status = es
 
 #====================================Caminho do arquivo JSON==================================================
@@ -18,17 +17,12 @@ caminho_arquivo = "base_demo.json"
 with open(caminho_arquivo, "r", encoding="utf-8") as f:
     dados = json.load(f)
 
-#print(dados)
-
-###########################################################
-
-# Nome do índice
+#=====================================Criação do índice========================================================
 index_name = "meu_indice"
 
 # Criar índice (opcional, se não existir)
 if not es.indices.exists(index=index_name):
     es.indices.create(index=index_name)
-
 
 if isinstance(dados, list):
     for i, doc in enumerate(dados):
@@ -39,12 +33,11 @@ else:
 
 print("Dados inseridos no Elasticsearch!")
 
-
-
 # Busca simples (todos os documentos)
 res = es.search(index=index_name, query={"match_all": {}})
 
 # Exibir resultados
 for hit in res["hits"]["hits"]:
     print(hit["_source"])
+
 
