2795f97006418acc64e1c76732fced655235b7ca
jamessonGIT <jamesson.usaf@gmail.com>
2025-12-03 10:28:21 -0300

    List all Elasticsearch indices
    Added functionality to list all indices from Elasticsearch.

--- conexao.py
+++ conexao.py
@@ -12,10 +12,18 @@ es_status = es(hosts = ["http://localhost:9200"],
 
 print(es_status.info())
 
+#===================================Listar todos os índices===================================================
+indices = es_status.cat.indices(format="json")  # retorna em formato JSON
+ 
+# Exibindo os índices
+for idx in indices:
+    print(f"Index: {idx['index']}, Health: {idx['health']}, Docs: {idx['docs.count']}, Size: {idx['store.size']}")
+
 #curl -XGET "localhost:9200/_cluster/health?pretty"
 
 #curl -XGET "localhost:9200/_cat/indices?v" 
 
 #curl -XGET "localhost:9200/nome_do_indice/_search?pretty" -H 'Content-Type: application/json' -d'
+
 
 
